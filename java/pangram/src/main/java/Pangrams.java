6dea7ba1781863bda1055dfec0d6b5f9617f6743
Steven Berlanga <zabawaba99@gmail.com>
2016-01-24 21:09:00 -0500

    Submitted Pangram in Java. Your submission can be found online at http://exercism.io/submissions/fc1ae55b90144e80b23dcf8e8691516e

--- java/pangram/src/main/java/Pangrams.java
+++ java/pangram/src/main/java/Pangrams.java
@@ -2,14 +2,14 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 public class Pangrams {
-	private static final ArrayList<String> ALL_THE_LETTERS = new ArrayList<>(Arrays.asList("a", "b", "c", "d", "e", "f", "g", "h", "i", "j",
-			"k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z" ));
+    private static final ArrayList<String> ALL_THE_LETTERS = new ArrayList<>(Arrays.asList("a", "b", "c", "d", "e", "f", "g", "h", "i", "j",
+            "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"));
 
-	public static boolean isPangram(String s) {
-		if (s == null) s = "";
+    public static boolean isPangram(String s) {
+        if (s == null) s = "";
 
-		// normalize case
-		final String candidate = s.toLowerCase();
-        return ALL_THE_LETTERS.stream().allMatch((s1) -> candidate.contains(s1));
-	}
+        // normalize case
+        final String candidate = s.toLowerCase();
+        return ALL_THE_LETTERS.stream().allMatch((letter) -> candidate.contains(letter));
+    }
 }
